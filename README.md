501cf7d6e2ba46e87198e3c00ccbbd8ccf930551
Ainhoa Prada <apt.code14@gmail.com>
2023-02-18 01:33:18 +0100

    working application

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# README-Maker
+
+                # Readme maker
+
+                ## Description
+
+                It makes readme files with node.js from the terminal :)
+
+                ## Contributors
+
+                Myself, node and npm pakages (fs ,inquirer) 
---
+++ index.js
@@ -0,0 +1,33 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+let title = '';
+let description = '';
+let contributors = '';
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is your project title?',
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'Could you provide some description about your project?',
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: 'Any contributor to this project?',
+            name: 'contributors'
+        }
+    ]).then((res) => {
+        console.log(res)
+        const readme = `
+                # ${res.title}\n
+                ## Description\n
+                ${res.description}\n
+                ## Contributors\n
+                ${res.contributors}`;
+        fs.writeFile('README.md', readme , (err) => err ? console.log(err) : console.log('Saved!'));
+    })
+
